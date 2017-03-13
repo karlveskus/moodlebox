08fca13c511250d87a8b9bda8a97f435ad92f75a
Karl Veskus <karl.veskus@gmail.com>
2017-03-13 23:07:00 +0200

    Adds contact form feedback endpoint

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 const users = require('./users');
+const feedback = require('./feedback');
 
 router.use('/users', users);
+router.use('/feedback', feedback);
 
 module.exports = router;
