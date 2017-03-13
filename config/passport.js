e4e9e4ea88886965217ee8f64d7ca9fac5921152
henry300 <henry.teigar@gmail.com>
2017-03-13 23:04:04 +0200

    Minor changes

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,3 @@
-"use strict";
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../models/user');
--- config/properties.js
+++ config/properties.js
@@ -2,7 +2,7 @@ const env = require('./env_variables.json')[process.env.NODE_ENV || 'development
 const secrets = require('./secrets.json');
 
 module.exports = {
-    port: env.port,
+    port: env.port | 3000,
     database: {
         host_name: 'MongoDb Atlas',
         src: 'mongodb://moodleBox:' + secrets.db_password + '@moodlebox-shard-00-00-rxgsj.mongodb.net:27017,moodlebox-shard-00-01-rxgsj.mongodb.net:27017,moodlebox-shard-00-02-rxgsj.mongodb.net:27017/MoodleBox?ssl=true&replicaSet=MoodleBox-shard-0&authSource=admin',
