ca7101af406f4696e5781e67350f673df262a925
Karl Veskus <karl.veskus@gmail.com>
2017-03-14 03:08:02 +0200

    Adds Feedback API Endpoint functionality

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,8 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Feedback = require('../models/feedback');
 
-router.get('/', function(req, res, next) {
-  res.send("Feedback endpoint")
+router.post('/', function(req, res, next) {
+
+    let newFeedback = new Feedback({
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message
+    });
+
+    Feedback.addFeedback(newFeedback, (err, feedback) => {
+        if(err){
+            res.json({ success: false, msg: "Failed to add feedback"});
+        } else {
+            res.json({ success: true, msg: "Feedback added"});
+        }
+    });
 });
 
 module.exports = router;
