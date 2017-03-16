a929ed0df141a3b6d6490b36aae70cbbdd283c51
Karl Veskus <karl.veskus@gmail.com>
2017-03-17 01:07:29 +0200

    Style LoggedinWrapper content components

--- angular-src/src/app/components/loggedin-wrapper/loggedin-wrapper.component.ts
+++ angular-src/src/app/components/loggedin-wrapper/loggedin-wrapper.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-loggedin-wrapper',
   templateUrl: './loggedin-wrapper.component.html',
-  styleUrls: ['./loggedin-wrapper.component.scss']
+  styleUrls: ['./loggedin-wrapper.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LoggedinWrapperComponent implements OnInit {
 
