3568ad2e1d29bcf43cc513026bafdf23d858ad3b
henry300 <henry.teigar@gmail.com>
2017-03-19 14:05:23 +0200

    Add authenticate middleware

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const jwt = require('jsonwebtoken');
+const properties = require('../config/properties');
+
 
 const users = require('./users');
 const feedback = require('./feedback');
 
+
+router.use(authenticateMiddleware(false));
 router.use('/users', users);
 router.use('/feedback', feedback);
+
+function authenticateMiddleware(mustBeAdmin) {
+    return function (req, res, next) {
+        let accessGranted = false;
+        let token = req.body.token || req.query.token || req.headers['x-access-token'];
+        if (token) {
+            jwt.verify(token, properties.jwt.secret, function(err, decoded) {
+                if (err) {
+                    res.sendFile(path.resolve('public/index.html'));
+                }
+                else {
+                    if (mustBeAdmin == decoded.admin) {
+                        next();
+                    }
+                }
+            });  
+        } else {
+            res.sendFile(path.resolve('public/index.html'));
+        }
+    }
+}
 
 module.exports = router;
