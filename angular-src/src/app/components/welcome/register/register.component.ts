dedee5b7077b0dab2a056902dbd1408d46f1c6a6
henry300 <henry.teigar@gmail.com>
2017-03-20 21:02:29 +0200

    Add registration form validation logic

--- angular-src/src/app/components/welcome/register/register.component.ts
+++ angular-src/src/app/components/welcome/register/register.component.ts
@@ -6,18 +6,59 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
-  registrationForm = new FormGroup({
-    email: new FormControl(null, Validators.required),
-    password: new FormControl(null, Validators.required),
-    passwordAgain: new FormControl(null, Validators.required)
-  });
+export class RegisterComponent implements OnInit{
+  problems: Object;
+  registrationForm: FormGroup;
+  email: FormControl;
+  password: FormControl;
+  passwordAgain: FormControl;
 
-  onRegisterSubmit() {
-    console.log(this.registrationForm);
+  constructor() {
+    this.email = new FormControl('',[Validators.required, Validators.pattern(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])+/)]);
+    this.password = new FormControl('',[Validators.required, Validators.minLength(5)]);
+    this.passwordAgain = new FormControl('',[Validators.required]);
   }
 
+  ngOnInit() {
+    this.registrationForm = new FormGroup({
+      email: this.email,
+      password: this.password,
+      passwordAgain: this.passwordAgain
+    });
+    this.updateProblems();
+    this.registrationForm.valueChanges.subscribe(data => {
+      this.updateProblems();
+    })
+  }
 
-  
+  onRegisterSubmit() {
+    this.updateProblems(); 
+    for (let property in this.registrationForm.controls) {
+      this.registrationForm.controls[property].markAsDirty();
+      this.registrationForm.controls[property].markAsTouched();
+    }
+
+    if (this.registrationForm.valid) {
+      if (this.registrationForm.controls['password'].value != this.registrationForm.controls['passwordAgain'].value) {
+        this.registrationForm.controls['passwordAgain'].setErrors({
+          match: false
+        });
+      } else {
+        console.log('MINE')
+      }
+    } 
+  }
+
+  updateProblems() {
+    let emailProblem = this.email.hasError('required') ? 'Väli on kohustuslik' : 'Ei ole emaili aadress';
+    let passwordProblem = this.password.hasError('required') ? 'Väli on kohustuslik' : 'Liiga lihtne parool';
+    let passwordAgainProblem = this.passwordAgain.hasError('required') ? 'Väli on kohustuslik' : 'Paroolid ei kattu';
+
+    this.problems = {
+      email: emailProblem,
+      password: passwordProblem,
+      passwordAgain: passwordAgainProblem
+    };
+  }
 
 }
