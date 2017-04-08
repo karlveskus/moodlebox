494c3118fad79ab079dbc627eba69eb71e1daca5
Karl Veskus <karl.veskus@gmail.com>
2017-04-08 19:56:28 +0300

    Add EST and ENG translations

--- angular-src/src/app/translate/lang-en.ts
+++ angular-src/src/app/translate/lang-en.ts
@@ -3,4 +3,58 @@ export const LANG_EN_NAME = 'en';
 export const LANG_EN_TRANS = {
     'WELCOME.WELCOME':              'Welcome to moodleBox!',
     'WELCOME.FASTEST_WAY':          'Fastest way to track your moodle tests',
+    'WELCOME.SAFE_PASSWORD':        'We don\'t save your password',
+    'WELCOME.DELETE_TESTS':         'You can always delete your tests',
+    'WELCOME.EASY_TO_USE':          'Easy to use tracking system for your moodle tests',
+    'WELCOME.ON_REGISTER':          'Review the',
+    'WELCOME.TERMS':                'Terms of Service',
+    'WELCOME.ACTIVE_USER':          'ACTIVE USERS',
+    'WELCOME.DIFFERENT_SUBJECTS':   'DIFFERENT SUBJECTS',
+    'WELCOME.MOODLE_TESTS':         'MOODLE TESTS',
+    'WELCOME.QUESTION':             'Have a question?',
+    'WELCOME.CONTACT_US':           'Contact us',
+    'WELCOME.SEND':                 'Send',
+    'WELCOME.NAME':                 'Name',
+    'WELCOME.EMAIL':                'Email',
+    'WELCOME.MESSAGE':              'Message',
+    'WELCOME.SUCCESSFUL_FEEDBACK':  'Thank you for the feedback!',
+
+    'LOGIN.LOGIN':                  'Log in',
+    'LOGIN.EMAIL':                  'Email',
+    'LOGIN.PASSWORD':               'Password',
+    'LOGIN.LOGIN_FACEBOOK':         'Log in with Facebook',
+    'LOGIN.LOGIN_GOOGLE':           'Log in with Google',
+    'LOGIN.NOT_USER':               'Not a user?',
+    'LOGIN.REGISTER':               'Register here!',
+
+    'REGISTER.REGISTER':            'Register',
+    'REGISTER.EMAIL':               'Email',
+    'REGISTER.PASSWORD':            'Password',
+    'REGISTER.PASSWORD_AGAIN':      'Password again',
+    'REGISTER.REGISTER_FACEBOOK':   'Register with Facebook',
+    'REGISTER.REGISTER_GOOGLE':     'Register with Google',
+    'REGISTER.ALREADY_USER':        'Already registered?',
+    'REGISTER.LOGIN':               'Log in here!',
+
+    'SIDEBAR.CONTENT':              'CONTENT',
+    'SIDEBAR.OVERVIEW':             'Overview',
+    'SIDEBAR.ADMIN_PANEL':          'Admin panel',
+    'SIDEBAR.UPLOADED_TESTS':       'Uploaded tests',
+    'SIDEBAR.USER_SETTINGS':        'User settings',
+    'SIDEBAR.LOGOUT':               'Log out',
+
+
+    'HOME.HEADING':                 'Overview',
+
+    'ADMIN.HEADING':                'Admin panel',
+
+    'TESTS.HEADING':                'Uploaded tests',
+
+    'SETTINGS.HEADING':             'User settings',
+
+
+
+    'NOT-FOUND.PAGE_NOT_FOUND':     'Page not found!',
+
+    'FOOTER.TERMS':                 'All rights reserved',
 };
--- angular-src/src/app/translate/lang-et.ts
+++ angular-src/src/app/translate/lang-et.ts
@@ -8,4 +8,52 @@ export const LANG_ET_TRANS = {
     'WELCOME.EASY_TO_USE':          'Mugav kasutuskeskkond testide vaatamiseks',
     'WELCOME.ON_REGISTER':          'Sisenedes nõustun',
     'WELCOME.TERMS':                'tingimustega',
+    'WELCOME.ACTIVE_USER':          'AKTIIVSET KASUTAJAT',
+    'WELCOME.DIFFERENT_SUBJECTS':   'ERINEVAT AINET',
+    'WELCOME.MOODLE_TESTS':         'MOODLE TESTI',
+    'WELCOME.QUESTION':             'Küsimus?',
+    'WELCOME.CONTACT_US':           'Võta meiega ühendust',
+    'WELCOME.SEND':                 'Saada',
+    'WELCOME.NAME':                 'Nimi',
+    'WELCOME.EMAIL':                'Email',
+    'WELCOME.MESSAGE':              'Sõnum',
+    'WELCOME.SUCCESSFUL_FEEDBACK':  'Täname tagasiside eest!',
+
+    'LOGIN.LOGIN':                  'Logi sisse',
+    'LOGIN.EMAIL':                  'Email',
+    'LOGIN.PASSWORD':               'Parool',
+    'LOGIN.LOGIN_FACEBOOK':         'Logi sisse Facebookiga',
+    'LOGIN.LOGIN_GOOGLE':           'Logi sisse Googlega',
+    'LOGIN.NOT_USER':               'Pole kasutaja?',
+    'LOGIN.REGISTER':               'Registreeru!',
+
+    'REGISTER.REGISTER':            'Registreeru',
+    'REGISTER.EMAIL':               'Email',
+    'REGISTER.PASSWORD':            'Parool',
+    'REGISTER.PASSWORD_AGAIN':      'Parool uuesti',
+    'REGISTER.REGISTER_FACEBOOK':   'Registreeru Facebookiga',
+    'REGISTER.REGISTER_GOOGLE':     'Registreeru Googlega',
+    'REGISTER.ALREADY_USER':        'Kasutaja olemas?',
+    'REGISTER.LOGIN':               'Logi sisse!',
+
+    'SIDEBAR.CONTENT':              'SISUKORD',
+    'SIDEBAR.OVERVIEW':             'Ülevaade',
+    'SIDEBAR.ADMIN_PANEL':          'Admin panel',
+    'SIDEBAR.UPLOADED_TESTS':       'Üles laetud testid',
+    'SIDEBAR.USER_SETTINGS':        'Konto seaded',
+    'SIDEBAR.LOGOUT':               'Logi välja',   
+
+
+    'HOME.HEADING':                 'Ülevaade',
+
+    'ADMIN.HEADING':                'Admin panel',
+
+    'TESTS.HEADING':                'Üles laetud testid',
+
+    'SETTINGS.HEADING':             'Konto seaded',
+
+
+    'NOT-FOUND.PAGE_NOT_FOUND':     'Otsitud lehte ei leitud!',
+
+    'FOOTER.TERMS':                  'Kõik õigused kaitstud',
 };
