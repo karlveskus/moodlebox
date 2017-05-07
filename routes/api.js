adc39de6d1fb9bbc95cdcb88439e6e7af04068c8
Karl Veskus <karl.veskus@gmail.com>
2017-05-07 16:09:32 +0300

    Refactor crawler

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const parser = require('../lib/parser.js');
 const cheerio = require('cheerio');
 const fs = require('fs-promise');
 
+const parser = require('../lib/parser.js');
+const crawler = require('../lib/crawler.js');
+
 const users = require('./users');
 const feedbacks = require('./feedbacks');
-const crawler = require('./crawler');
 
 router.use('/users', users);
 router.use('/feedbacks', feedbacks);
-router.use('/crawler', crawler);
 
 // This route is just for testing
 router.get('/parser', (req, res, next) => {
     fs.readFile('./lib/testHtml.html').then((sisu)=>{
         res.send(parser.parseHtml(sisu));
     });
+});
+
+router.post('/crawler', (req, res) => { 
+  crawler.run(req, res);
 });
 
 module.exports.router = router;
